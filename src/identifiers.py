ddafec950d9c9499de12e29a647074ed74b9da5c
Jaroslaw Jaworski <jjaworski@craftware.biz>
2026-05-03 19:16:19 +0200

    feat: Add UUID implementation for models

---
+++ src/identifiers.py
@@ -0,0 +1,15 @@
+from typing import NewType
+from uuid import UUID, uuid4
+
+UserId = NewType("UserId", UUID)
+CategoryId = NewType("CategoryId", UUID)
+TopicId = NewType("TopicId", UUID)
+QuizId = NewType("QuizId", UUID)
+QuestionId = NewType("QuestionId", UUID)
+ChoiceId = NewType("ChoiceId", UUID)
+
+def new_user_id() -> UserId: return UserId(uuid4())
+def new_category_id() -> CategoryId: return CategoryId(uuid4())
+def new_topix_id() -> TopicId: return TopicId(uuid4())
+def new_quiz_id() -> QuizId: return QuizId(uuid4())
+def new_choice_id() -> ChoiceId: return ChoiceId(uuid4())
