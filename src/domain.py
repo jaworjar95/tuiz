d790d5a96d33bab9e3ec86e3311459a15cb8553b
Jaroslaw Jaworski <jjaworski@craftware.biz>
2026-05-07 00:11:16 +0200

    feat(domain): Create new domains for the quiz data model

---
+++ src/domain.py
@@ -0,0 +1,41 @@
+from pydantic import BaseModel, ConfigDict, Field
+from datetime import datetime, timezone
+import identifiers
+
+def utc_field_now(): # TODO: Could be moved to seprate file like fields.py
+    return Field(default_factory=lambda: datetime.now(timezone.utc), frozen=True)
+
+class User(BaseModel):
+    id: identifiers.UserId = identifiers.new_user_id()
+    created_date: datetime = utc_field_now()
+    name: str
+
+class Category(BaseModel):
+    id: identifiers.CategoryId = identifiers.new_category_id()
+    created_date: datetime = utc_field_now()
+    name: str
+
+class Topic(BaseModel):
+    id: identifiers.TopicId = identifiers.new_topic_id()
+    category_id: identifiers.CategoryId
+    created_date: datetime = utc_field_now()
+    name: str
+
+class Quiz(BaseModel):
+    id: identifiers.QuizId = identifiers.new_quiz_id()
+    created_date: datetime = utc_field_now()
+    name: str
+
+class Question(BaseModel):
+    id: identifiers.QuestionId = identifiers.new_question_id()
+    category_id: identifiers.CategoryId
+    topic_id: identifiers.TopicId | None = None
+    created_date: datetime = utc_field_now()
+    content: str
+
+class Choice(BaseModel):
+    id: identifiers.ChoiceId = identifiers.new_choice_id()
+    question_id: identifiers.QuestionId
+    created_date: datetime = utc_field_now()
+    content: str
+
--- src/identifiers.py
+++ src/identifiers.py
@@ -10,6 +10,7 @@ ChoiceId = NewType("ChoiceId", UUID)
 
 def new_user_id() -> UserId: return UserId(uuid4())
 def new_category_id() -> CategoryId: return CategoryId(uuid4())
-def new_topix_id() -> TopicId: return TopicId(uuid4())
+def new_topic_id() -> TopicId: return TopicId(uuid4())
+def new_question_id() -> QuestionId: return QuestionId(uuid4())
 def new_quiz_id() -> QuizId: return QuizId(uuid4())
 def new_choice_id() -> ChoiceId: return ChoiceId(uuid4())
