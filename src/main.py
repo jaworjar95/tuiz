821267756803de83383d40321a75047028d1ab30
Jaroslaw Jaworski <jjaworski@craftware.biz>
2026-05-07 00:12:15 +0200

    test(domain): Test if the initial quiz dama model is correctly set up

--- src/main.py
+++ src/main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from tuiz!")
+from domain import User, Category, Topic, Quiz, Question, Choice
 
+def main():
+    user = User(name="Jarek")
+    quiz = Quiz(name="Quiz Testowy")
+    category = Category(name="History")
+    topic = Topic(category_id=category.id, name="WW1")
+    question = Question(category_id=category.id, topic_id=topic.id, content="Who won the World War I?")
+    choice_1 = Choice(question_id=question.id, content="France")
+    
+    print(f"User with id {user.id} will now answer question with id {question.id}")
+    print(f"The quiz was created at {quiz.created_date}")
+    print(f"The first question is: {question.content}")
+    print(f"The only correct answer is: {choice_1.content}")
 
 if __name__ == "__main__":
     main()
